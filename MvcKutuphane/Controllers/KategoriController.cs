71b163fd1fda75098a5e5e53c880746e5fd0463a
Developer <Developer@DEV>
2025-10-13 17:18:24 +0300

    Add project files.

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+       DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORİ.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(TBLKATEGORİ p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("KategoriEkle");
+            }
+            db.TBLKATEGORİ.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
