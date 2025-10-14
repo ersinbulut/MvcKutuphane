4399ed4efa14958519043e5e2febc599801ff9ce
Developer <Developer@DEV>
2025-10-14 13:29:28 +0300

    38. Üye Silme ve Güncelleme

---
+++ MvcKutuphane/Controllers/UyeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+using PagedList;
+
+namespace MvcKutuphane.Controllers
+{
+    public class UyeController : Controller
+    {
+        DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        // GET: Uye
+        public ActionResult Index(int sayfa=1)
+        {
+            var degerler = db.TBLUYELER.ToList().ToPagedList(sayfa, 3);
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult UyeEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UyeEkle(TBLUYELER p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("UyeEkle");
+            }
+            db.TBLUYELER.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UyeSil(int id)
+        {
+            var uye = db.TBLUYELER.Find(id);
+            db.TBLUYELER.Remove(uye);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UyeGuncelle(int id)
+        {
+            var uye = db.TBLUYELER.Find(id);
+            return View("UyeGuncelle", uye);
+        }
+        [HttpPost]
+        public ActionResult UyeGuncelle(TBLUYELER p)
+        {
+            var uye = db.TBLUYELER.Find(p.ID);
+            uye.AD = p.AD;
+            uye.SOYAD = p.SOYAD;
+            uye.FOTOGRAF = p.FOTOGRAF;
+            uye.MAIL = p.MAIL;
+            uye.KULLANICIADI = p.KULLANICIADI;
+            uye.SIFRE = p.SIFRE;
+            uye.TELEFON = p.TELEFON;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
