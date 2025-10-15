b26750b2f8781aad9280dda9e3cca03a9d9edc03
Developer <Developer@DEV>
2025-10-15 09:40:48 +0300

    40. Kitap Ödünç Verme İşlemi

---
+++ MvcKutuphane/Controllers/OduncController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class OduncController : Controller
+    {
+        DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        // GET: Odunc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult OduncVer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult OduncVer(TBLHAREKET p)
+        {
+            db.TBLHAREKET.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult OduncAl()
+        {
+            return View();
+        }
+    }
+}
