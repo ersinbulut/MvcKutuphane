be57a673a586f04a7fed18c615ff358c02255edf
Developer <Developer@DEV>
2025-10-31 09:53:28 +0300

    93. Mail Adresini Profile Taşıma

---
+++ MvcKutuphane/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+using System.Web.Security;
+
+namespace MvcKutuphane.Controllers
+{
+    public class LoginController : Controller
+    {
+        DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        // GET: Login
+        [HttpGet]
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GirisYap(TBLUYELER p)
+        {
+            var bilgiler = db.TBLUYELER.FirstOrDefault(x => x.MAIL == p.MAIL && x.SIFRE == p.SIFRE);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.MAIL,false);
+                return RedirectToAction("Index", "Panelim");
+            }
+            else
+            {
+                return View();
+            }
+         
+        }
+    }
+}
