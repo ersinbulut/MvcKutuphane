d9ab85120710d5df5875169a2ee63b488c55e28c
Developer <Developer@DEV>
2025-11-18 14:59:39 +0300

    145. Gelen Kutusunda Neler Yapacağız

--- MvcKutuphane/Controllers/PanelimController.cs
+++ MvcKutuphane/Controllers/PanelimController.cs
@@ -17,7 +17,30 @@ namespace MvcKutuphane.Controllers
         public ActionResult Index()
         {
             var uyemail = (string)Session["Mail"];
-            var degerler = db.TBLUYELER.FirstOrDefault(z => z.MAIL == uyemail);
+            // degerler = db.TBLUYELER.FirstOrDefault(z => z.MAIL == uyemail);
+            var degerler = db.TBLDUYURULAR.ToList();
+            var d1=db.TBLUYELER.Where(x => x.MAIL== uyemail).Select(y=>y.AD).FirstOrDefault();
+            var d2=db.TBLUYELER.Where(x => x.MAIL== uyemail).Select(y=>y.SOYAD).FirstOrDefault();
+            var d3=db.TBLUYELER.Where(x => x.MAIL== uyemail).Select(y=>y.FOTOGRAF).FirstOrDefault();
+            var d4=db.TBLUYELER.Where(x => x.MAIL== uyemail).Select(y=>y.KULLANICIADI).FirstOrDefault();
+            var d5=db.TBLUYELER.Where(x => x.MAIL== uyemail).Select(y=>y.OKUL).FirstOrDefault();
+            var d6=db.TBLUYELER.Where(x => x.MAIL== uyemail).Select(y=>y.TELEFON).FirstOrDefault();
+            var d7=db.TBLUYELER.Where(x => x.MAIL== uyemail).Select(y=>y.MAIL).FirstOrDefault();
+            var uyeid= db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.ID).FirstOrDefault();
+            var d8=db.TBLHAREKET.Where(x=>x.UYE==uyeid).Count();
+            var d9 = db.TBLMESAJLAR.Where(x => x.ALICI == uyemail).Count();
+            var d10 = db.TBLDUYURULAR.Count();
+
+            ViewBag.d1 = d1;
+            ViewBag.d2 = d2;
+            ViewBag.d3 = d3;
+            ViewBag.d4 = d4;
+            ViewBag.d5 = d5;
+            ViewBag.d6 = d6;
+            ViewBag.d7 = d7;
+            ViewBag.d8 = d8;
+            ViewBag.d9 = d9;
+            ViewBag.d10 = d10;
             return View(degerler);
         }
         [HttpPost]
@@ -27,6 +50,7 @@ namespace MvcKutuphane.Controllers
             var uye=db.TBLUYELER.FirstOrDefault(x=>x.MAIL ==kullanici);
             uye.SIFRE = p.SIFRE;
             uye.AD = p.AD;
+            uye.SOYAD = p.SOYAD;
             uye.FOTOGRAF = p.FOTOGRAF;
             uye.OKUL = p.OKUL;
             uye.KULLANICIADI = p.KULLANICIADI;
@@ -70,6 +94,19 @@ namespace MvcKutuphane.Controllers
         {
             FormsAuthentication.SignOut();
             return RedirectToAction("GirisYap", "Login");
+        }
+
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult Partial2()
+        {
+            var mail = (string)Session["Mail"];
+            var uye = db.TBLUYELER.Where(x => x.MAIL == mail).Select(y => y.ID).FirstOrDefault();
+            var uyebul=db.TBLUYELER.Find(uye);
+            return PartialView("Partial2",uyebul);
         }
     }
 }
